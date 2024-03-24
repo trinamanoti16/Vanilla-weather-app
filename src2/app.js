a934c1d11374d94885f680bf6ad7150b41362116
trinamanoti16 <trinamanoti16@gmail.com>
2024-03-24 22:28:24 +0300

    Updated API on js file

--- src2/app.js
+++ src2/app.js
@@ -1,3 +1,17 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+searchCity("Paris");
+
+function searchCity(city) {
+  let apiKey = `3844c0abc40t8o0630ea1bbf9f664d0b`;
+  let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
