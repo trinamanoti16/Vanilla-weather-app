5988ccc629a5acccc27cc57fbc2850aa7916acec
trinamanoti16 <trinamanoti16@gmail.com>
2024-03-24 23:49:21 +0300

    Updated the icon html, css and js

--- src2/app.js
+++ src2/app.js
@@ -7,6 +7,10 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
+      class="weather-app-icon" />`;
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
